ef601030dcb64f82e285d01631006dcc8e658b7b
Rody10 <rodneysimbatholanah@gmail.com>
2025-02-18 18:36:55 +0200

    second commit

---
+++ ExcerciseTracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExcerciseTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
